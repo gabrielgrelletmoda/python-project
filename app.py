63b83a2741d7f47ab28d237bbfae212cd17285b3
gabrielmoda <gabriel.grellet.moda@gmail.com>
2024-10-09 15:18:25 -0300

    projeto

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 def criar_registro_pipefy(dados, PIPEFY_TOKEN=None, PIPEFY_API_URL=None):
     query = """
-    mutation {
+   mutation {
   createTableRecord(input: {table_id: ID, title: "New record", fields_attributes: {field_id: "text", field_value: "New value for this record field"}}) {
     table_record {
       id
     }
   }
 }
-    """ % (dados['nome'], dados['nome'], dados['email'])
+    """
 
     headers = {
         'Authorization': PIPEFY_TOKEN,''
@@ -29,6 +29,7 @@ def registrar(PIPEFY_TOKEN=None, PIPEFY_API_URL=None):
     dados = request.json
     resultado = criar_registro_pipefy(dados, PIPEFY_TOKEN, PIPEFY_API_URL)
     return jsonify(resultado)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
